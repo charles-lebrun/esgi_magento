34d799752c8cc424460d27154d2fb62ced5376ba
Charles LEBRUN <charles.lebrun@tiwas-company.com>
2017-05-11 08:45:20 +0200

    gestion du slug

--- src/app/code/local/Esgi/Brand/Model/Resource/Brand.php
+++ src/app/code/local/Esgi/Brand/Model/Resource/Brand.php
@@ -11,4 +11,24 @@ class Esgi_Brand_Model_Resource_Brand extends Mage_Core_Model_Resource_Db_Abstra
         $this->_init('esgi_brand/brand', 'entity_id');
     }
 
+	protected function _beforeSave(Mage_Core_Model_Abstract $object)
+	{
+
+		/**
+		 * create a slug
+		 */
+		$this->_prepareUrlKey($object);
+
+		parent::_beforeSave($object);
+
+		return $this;
+	}
+
+
+	protected function _prepareUrlKey(&$object)
+	{
+		$object->setSlug(Mage::getModel('catalog/product_url')->formatUrlKey( $object->getName() ));
+		return $this;
+	}
+
 }
